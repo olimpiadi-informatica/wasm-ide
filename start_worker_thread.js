4775a580d6347e940dec417bca20f066035f42df
Luca Versari <veluca93@gmail.com>
2024-06-24 12:30:51 +0200

    Speed up thread creation

--- start_worker_thread.js
+++ start_worker_thread.js
@@ -2,7 +2,10 @@ import init, {threadRunFn} from './worker.js';
 
 self.onmessage = async (msg) => {
   let data = msg['data'];
-  let wasm = await init(data['module'], data['memory']);
-  threadRunFn(data['closure'], data['arg']);
-  wasm.__wbindgen_thread_destroy();
+  if ('module' in data) {
+    await init(data['module'], data['memory']);
+  }
+  if ('workerIndex' in data) {
+    threadRunFn(data['workerIndex'], data['arg']);
+  }
 };
--- worker/src/thread.rs
+++ worker/src/thread.rs
@@ -1,8 +1,19 @@
+use std::sync::{Mutex, OnceLock};
+
 use js_sys::Object;
-use log::{debug, warn};
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::{closure::Closure, JsCast, JsValue};
-use web_sys::{DedicatedWorkerGlobalScope, Worker, WorkerOptions, WorkerType};
+use log::debug;
+use send_wrapper::SendWrapper;
+use wasm_bindgen::{prelude::*, JsValue};
+use web_sys::{Worker, WorkerOptions, WorkerType};
+
+static AVAILABLE_WORKERS: OnceLock<Mutex<Vec<usize>>> = OnceLock::new();
+
+struct WorkerData {
+    worker: SendWrapper<Worker>,
+    next_task: Option<Box<dyn FnOnce(JsValue) + Send + 'static>>,
+}
+
+static WORKER_DATA: OnceLock<Mutex<Vec<WorkerData>>> = OnceLock::new();
 
 pub async fn spawn_simple<F>(closure: F)
 where
@@ -11,66 +22,83 @@ where
     spawn(move |_| closure(), JsValue::null()).await;
 }
 
-pub async fn spawn<F>(closure: F, arg: JsValue) -> Worker
+pub async fn spawn<F>(closure: F, arg: JsValue)
 where
     F: FnOnce(JsValue) + Send + 'static,
 {
     debug!("spawning thread");
-    let fun: Box<dyn FnOnce(JsValue) + Send + 'static> = Box::new(closure);
 
-    let mut options = WorkerOptions::default();
-    options.type_(WorkerType::Module);
-    let worker = Worker::new_with_options("./start_worker_thread.js", &options)
-        .expect("couldn't start thread");
+    let worker_id = {
+        let available_worker = AVAILABLE_WORKERS
+            .get_or_init(|| Mutex::new(vec![]))
+            .lock()
+            .unwrap()
+            .pop();
+        if let Some(w) = available_worker {
+            w
+        } else {
+            let mut options = WorkerOptions::default();
+            options.type_(WorkerType::Module);
+            let worker = Worker::new_with_options("./start_worker_thread.js", &options)
+                .expect("couldn't start thread");
+
+            let mut msg = Object::new();
+            js_sys::Reflect::set(&mut msg, &"module".into(), &wasm_bindgen::module())
+                .expect("could not set module");
+            js_sys::Reflect::set(&mut msg, &"memory".into(), &wasm_bindgen::memory())
+                .expect("could not set memory");
+            worker
+                .post_message(&msg)
+                .expect("failed sending init message to worker");
+            let mut workers = WORKER_DATA
+                .get_or_init(|| Mutex::new(vec![]))
+                .lock()
+                .unwrap();
+            workers.push(WorkerData {
+                worker: SendWrapper::new(worker),
+                next_task: None,
+            });
+            workers.len() - 1
+        }
+    };
 
     let mut msg = Object::new();
-    js_sys::Reflect::set(&mut msg, &"module".into(), &wasm_bindgen::module())
-        .expect("could not set module");
-    js_sys::Reflect::set(&mut msg, &"memory".into(), &wasm_bindgen::memory())
-        .expect("could not set memory");
-    // This variable must stay alive until we receive a message from the worker.
-    let fun: *mut dyn FnOnce(JsValue) = Box::into_raw(fun);
-    js_sys::Reflect::set(
-        &mut msg,
-        &"closure".into(),
-        &JsValue::from(&fun as *const _ as usize),
-    )
-    .expect("could not set closure");
+    js_sys::Reflect::set(&mut msg, &"workerIndex".into(), &JsValue::from(worker_id))
+        .expect("could not set worker_id");
     js_sys::Reflect::set(&mut msg, &"arg".into(), &arg).expect("could not set arg");
-    let (send, recv) = async_channel::bounded(1);
-    worker.set_onmessage(Some(
-        Closure::<dyn Fn(_)>::new(move |_: JsValue| {
-            debug!("thread started");
-            if let Err(_) = send.try_send(()) {
-                warn!("got multiple messages from spawned thread");
-            }
-        })
-        .into_js_value()
-        .unchecked_ref(),
-    ));
-    worker
+
+    let mut workers = WORKER_DATA
+        .get_or_init(|| Mutex::new(vec![]))
+        .lock()
+        .unwrap();
+
+    workers[worker_id].next_task = Some(Box::new(closure));
+    workers[worker_id]
+        .worker
         .post_message(&msg)
-        .expect("could not send message to worker");
-    recv.recv().await.expect("thread failed to start");
-    worker
+        .expect("failed sending task message to worker");
 }
 
-// # Safety
-// Must be called in a worker spawned by `spawn`.
-// `closure` must be the address of a pointer to a &mut dyn Fn(JsValue) + Send + 'static,
-// which must stay alive until a message is sent back from the worker.
-// The &mut dyn Fn(JsValue) + Send + 'static must be obtained via Box::leak.
 #[wasm_bindgen(js_name = "threadRunFn")]
-pub unsafe fn thread_run_fun(closure: usize, arg: JsValue) {
+pub fn thread_run_fun(worker_id: usize, arg: JsValue) {
     debug!("thread starting...");
-    let closure = (closure as *const &mut dyn FnOnce(JsValue)).read();
-    let closure = Box::from_raw(closure);
 
-    js_sys::global()
-        .dyn_into::<DedicatedWorkerGlobalScope>()
-        .expect("not a worker")
-        .post_message(&JsValue::null())
-        .expect("could not signal readiness");
+    let closure = {
+        let mut workers = WORKER_DATA
+            .get_or_init(|| Mutex::new(vec![]))
+            .lock()
+            .unwrap();
+        workers[worker_id]
+            .next_task
+            .take()
+            .expect("sent message to worker without a closure to run")
+    };
 
-    closure(arg)
+    closure(arg);
+
+    AVAILABLE_WORKERS
+        .get_or_init(|| Mutex::new(vec![]))
+        .lock()
+        .unwrap()
+        .push(worker_id);
 }
