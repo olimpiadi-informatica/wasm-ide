3151c42c1af2c5a650f0b268ee05705bb5c4bdbd
Luca Versari <veluca93@gmail.com>
2024-06-27 18:40:29 +0200

    Fix race condition in thread creation.

--- start_worker_thread.js
+++ start_worker_thread.js
@@ -1,11 +1,18 @@
 import init, {threadRunFn} from './worker.js';
 
+let setInitialized;
+let initialized = new Promise((resolve, _) => {
+  setInitialized = resolve;
+});
+
 self.onmessage = async (msg) => {
   let data = msg['data'];
   if ('module' in data) {
     await init(data['module'], data['memory']);
+    setInitialized();
   }
   if ('workerIndex' in data) {
+    await initialized;
     threadRunFn(data['workerIndex'], data['arg']);
   }
 };
